90210e6d64a6ebbc99a1ac518cfebb6a58e7648c
Risshab Singla <risshab2singla@gmail.com>
2024-12-01 14:06:53 +0530

    Admin Nav Bar

--- Frontend/src/app/components/shared/navbar/admin-nav-bar/admin-nav-bar.component.ts
+++ Frontend/src/app/components/shared/navbar/admin-nav-bar/admin-nav-bar.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-nav-bar',
   templateUrl: './admin-nav-bar.component.html',
-  styleUrl: './admin-nav-bar.component.scss'
+  styleUrl: './admin-nav-bar.component.scss',
 })
 export class AdminNavBarComponent {
+  isMenuOpen = false;
+  currentRole: 'admin' | 'user' | 'guest' = 'guest';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentRole = this.authService.getCurrentUserRole();
+  }
+
+  // Logout the user and redirect to the login page
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+
+  // Toggle the sidebar menu for mobile view
+  toggleSidebar() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
