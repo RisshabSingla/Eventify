5cfeb657ece3e18b91ac049ba1124a32a6f93dce
Risshab Singla <risshab2singla@gmail.com>
2024-12-03 12:17:02 +0530

    Feedback View (Dummy Data)

--- Frontend/src/app/components/feedback/feedback-view/feedback-view.component.ts
+++ Frontend/src/app/components/feedback/feedback-view/feedback-view.component.ts
@@ -8,6 +8,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FeedbackViewComponent {
   feedbackId = '';
+
+  eventDetails = {
+    title: 'Annual Tech Conference 2024',
+  };
+
+  feedbackDetails = {
+    id: 'FB-238',
+    submissionDate: '2024-12-01',
+    userName: 'John Doe',
+    rating: 4, // Star rating: 4 out of 5
+    feedbackText:
+      'The event was great, but the technical difficulties during the keynote were frustrating. Would love to see improvements in the AV setup next time.',
+  };
+
   constructor(private _ar: ActivatedRoute) {
     this.feedbackId = _ar.snapshot.params['feedbackId'];
     console.log(this.feedbackId);
