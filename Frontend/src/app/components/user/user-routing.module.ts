243bad107e8bb8cb5d7bb77b102b4f9e992d1754
Risshab Singla <risshab2singla@gmail.com>
2024-12-03 14:30:13 +0530

    Deleted User Upcoming Events

--- Frontend/src/app/components/user/user-routing.module.ts
+++ Frontend/src/app/components/user/user-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { UserDashboardRegisteredComponent } from './user-dashboard-registered/user-dashboard-registered.component';
 import { UserDashboardExploreComponent } from './user-dashboard-explore/user-dashboard-explore.component';
-import { UserDashboardUpcomingComponent } from './user-dashboard-upcoming/user-dashboard-upcoming.component';
 import { UserDashboardQrCodeComponent } from './user-dashboard-qr-code/user-dashboard-qr-code.component';
 import { UserDashboardGiveFeedbackComponent } from './user-dashboard-give-feedback/user-dashboard-give-feedback.component';
 import { UserDashboardViewFeedbackComponent } from './user-dashboard-view-feedback/user-dashboard-view-feedback.component';
@@ -11,6 +10,7 @@ import { UserDashboardItemsComponent } from './user-dashboard-items/user-dashboa
 import { EventListComponent } from '../events/event-list/event-list.component';
 import { EventDetailComponent } from '../events/event-detail/event-detail.component';
 import { EventAttendeeListComponent } from '../events/event-attendee-list/event-attendee-list.component';
+import { FeedbackFormComponent } from '../feedback/feedback-form/feedback-form.component';
 
 const routes: Routes = [
   {
@@ -38,9 +38,21 @@ const routes: Routes = [
         ],
       },
       { path: 'registered', component: UserDashboardRegisteredComponent },
-      { path: 'upcoming', component: UserDashboardUpcomingComponent },
+
       { path: 'qr-code', component: UserDashboardQrCodeComponent },
-      { path: 'give-feedback', component: UserDashboardGiveFeedbackComponent },
+      {
+        path: 'give-feedback',
+        children: [
+          {
+            path: '',
+            component: UserDashboardGiveFeedbackComponent,
+          },
+          {
+            path: ':id',
+            component: FeedbackFormComponent,
+          },
+        ],
+      },
       { path: 'view-feedback', component: UserDashboardViewFeedbackComponent },
       {
         path: 'event',
--- Frontend/src/app/components/user/user.module.ts
+++ Frontend/src/app/components/user/user.module.ts
@@ -5,7 +5,6 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard.componen
 import { UserDashboardItemsComponent } from './user-dashboard-items/user-dashboard-items.component';
 import { UserDashboardExploreComponent } from './user-dashboard-explore/user-dashboard-explore.component';
 import { UserDashboardRegisteredComponent } from './user-dashboard-registered/user-dashboard-registered.component';
-import { UserDashboardUpcomingComponent } from './user-dashboard-upcoming/user-dashboard-upcoming.component';
 import { UserDashboardQrCodeComponent } from './user-dashboard-qr-code/user-dashboard-qr-code.component';
 import { UserDashboardGiveFeedbackComponent } from './user-dashboard-give-feedback/user-dashboard-give-feedback.component';
 import { UserDashboardViewFeedbackComponent } from './user-dashboard-view-feedback/user-dashboard-view-feedback.component';
@@ -16,7 +15,6 @@ import { UserDashboardViewFeedbackComponent } from './user-dashboard-view-feedba
     UserDashboardItemsComponent,
     UserDashboardExploreComponent,
     UserDashboardRegisteredComponent,
-    UserDashboardUpcomingComponent,
     UserDashboardQrCodeComponent,
     UserDashboardGiveFeedbackComponent,
     UserDashboardViewFeedbackComponent,
