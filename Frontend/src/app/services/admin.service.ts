3e722d2e40e82407f1b6bd47f0366970c964026d
Risshab Singla <risshab2singla@gmail.com>
2024-12-06 21:32:27 +0530

    Frontend Integration Admin Created and All Events

--- Frontend/src/app/services/admin.service.ts
+++ Frontend/src/app/services/admin.service.ts
@@ -9,25 +9,52 @@ import {
   ADMIN_DASHBOARD_REPORTS_DATA,
 } from './dummy_data';
 import { Items } from '../model/admin/Items';
-import { Observable, of } from 'rxjs';
+import { forkJoin, map, Observable, of } from 'rxjs';
 import { EventManagement } from '../model/admin/Event_Management';
 import { EventAnalytics } from '../model/admin/Event_Analytics';
 import { EventAttendance } from '../model/admin/Event_Attendance';
 import { AdminNotification } from '../model/admin/Notifications';
 import { EventFeedback } from '../model/admin/Event_Feedback';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Event } from '../model/admin/Event';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
-  constructor() {}
+  apiEndpoint = `http://localhost:8080/`;
+
+  constructor(private http: HttpClient) {}
 
   getDashboardItems(): Observable<Items> {
     return of(ADMIN_DASHBOARD_DATA);
   }
 
   getEventManagementData(): Observable<EventManagement> {
-    return of(ADMIN_DASHBOARD_EVENT_MANAGEMENT_DATA);
+    const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    const token = currentUser.token;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    const allEvents = this.http.get<Event[]>(
+      `${this.apiEndpoint}events/getAllEvents`,
+      { headers }
+    );
+    const createdByAdminEvents = this.http.get<Event[]>(
+      `${this.apiEndpoint}events/getMyCreatedEvents`,
+      { headers }
+    );
+
+    return forkJoin([createdByAdminEvents, allEvents]).pipe(
+      map(([createdByAdminEvents, allEvents]) => ({
+        createdByAdminEvents: createdByAdminEvents,
+        allEvents: allEvents,
+      }))
+    );
+    // return of(ADMIN_DASHBOARD_EVENT_MANAGEMENT_DATA);
   }
 
   getEventAnalyticsData(): Observable<EventAnalytics> {
